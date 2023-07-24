d1d8c51a944ece516e2ad3a4f007295c5fb3b8a1
Degreatjoe <jgreat770@gmail.com>
2023-07-24 21:22:35 +0100

    grade calculator

---
+++ grade_calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Write a program that determines the letter grade for a course
+ * according to the following scale:
+ * •	A >= 90
+ * •	B >= 80
+ * •	C >= 70
+ * •	D >= 60
+ * •	F < 60
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+
